814b66ce5047ab9e0e20668656a05c5982754baf
HanzzAltar <nnzz24h@gmail.com>
2024-12-09 14:40:41 +0700

    Install.sh
    command:Bash install.sh

---
+++ Install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash               
+echo "system :"; pkg update && pkg upgrade
+echo "system :"; pkg install git
+echo "system :"; pkg install nodejs
+echo "system :"; node spam.js
+
+# Hapus skrip ini setelah dijalankan
+rm -- "$0"
